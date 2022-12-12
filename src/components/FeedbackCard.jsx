ccbd3444e88e8dea302cbf47e35b35ad35b70640
M Faisal Hayat <m.faisalhayat3@gmail.com>
2022-12-12 20:06:52 +0500

    testimonials are done

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -2,8 +2,22 @@ import { quotes } from "../assets";
 
 function FeedbackCard(props){
   return(
-    <div>
-      FeedbackCard
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img src={quotes} alt="Image" className="w-[42px] h-[27px] object-contain" />
+      <p className="font-poppins fonts-normal text-[18px] leading-[32px] text-white my-10">
+        {props.content}
+      </p>
+      <div className="flex flex-row">
+        <img src={props.img} alt="Image" className="w-[48px] h-[48px] rounded-full" />
+        <div className="flex flex-col ml-4">
+          <h4 className="text-white font-poppins font-semibold text-[20px] leading-[32px]">
+            {props.name}
+          </h4>
+          <p className="text-dimWhite font-poppins font-normal text-[16px] leading-[20px]">
+            {props.title}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,9 +4,26 @@ import FeedbackCard from "./FeedbackCard";
 
 function Testimonials(props){
   return(
-    <div>
-      Testimonials
-    </div>
+    <section className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`} id="clients">
+      {/* TODO: we need to complete it */}
+      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-ful blue__gradient" />
+      <div className="w-full flex justify-between items-centermd:flex-row flex-col sm:mb-16 mb-6 relative z-[1px]">
+        <h1 className={styles.heading2}>
+          What are <br className="sm:block hidden" /> people saying about us 
+        </h1>
+        <div className="w-full md:mt-0 mt-6">
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+        {feedback.map((card) => (
+          <FeedbackCard key={card.id} {...card} />
+        ))}
+      </div>
+    </section>
   )
 }
 
