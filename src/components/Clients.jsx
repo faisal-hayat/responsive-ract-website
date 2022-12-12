10b74540b8658e3f9be39c88ab7daed2f7a20f50
M Faisal Hayat <m.faisalhayat3@gmail.com>
2022-12-12 20:30:29 +0500

    clients section done

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -3,9 +3,15 @@ import styles from "../style";
 
 function Clients(props){
   return (
-    <div>
-      Clients
-    </div>
+   <section className={`${styles.flexCenter} my-4`} id="">
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+          <img src={client.logo} alt="logo image" className="sm:w-[192px] w-[100px] object-contain" />
+        </div>
+      ))}
+    </div>   
+   </section>
   )
 }
 
